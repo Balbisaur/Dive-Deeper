441aa4e3173389aa1b4e1663527418899647bba1
Balbisaur <albi3479@yahoo.com>
2024-04-10 13:17:13 -0500

    first commit

---
+++ Task1_2_The_Greatest_Showdown.py
@@ -0,0 +1,16 @@
+number = '101'
+if number < '100':
+    print('number is less than 100')
+elif number == '78':
+    print('equal to 78.')
+else:
+    print('number is greater.')
+
+
+number = '23'
+if number < '36':
+    print('number is less')
+elif number == '78':
+    print('equal to 78.')
+else:
+    print('number is greater.')
